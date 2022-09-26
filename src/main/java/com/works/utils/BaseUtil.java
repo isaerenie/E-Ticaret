5a5917bdfb8dc725777ae1df2b37e6971d920207
isa Eren <105389265+Eren100@users.noreply.github.com>
2022-09-26 14:07:42 +0300

    Services,controllers and repositories

---
+++ src/main/java/com/works/utils/BaseUtil.java
@@ -0,0 +1,27 @@
+package com.works.utils;
+
+
+import org.apache.tomcat.util.http.fileupload.FileUtils;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
+
+public class BaseUtil {
+
+
+    public static String encodeImage(Path pathToImage) throws Exception {
+        byte[] bytes = Files.readAllBytes(pathToImage);
+        String base64EncodedImageBytes = Base64.getEncoder().encodeToString(bytes);
+        return base64EncodedImageBytes;
+    }
+
+
+    public static void decodeImage(String base64EncodedString) throws Exception {
+        byte[] decode = Base64.getDecoder().decode(base64EncodedString);
+        Path path = Paths.get("/home/mkyong/test/phone2.png");
+        Files.write(path, decode);
+    }
+}
